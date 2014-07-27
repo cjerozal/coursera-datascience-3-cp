153373f60d1810af7f80b6ad531d4213f696eed3
Cheryl <cjerozal@atlassian.com>
2014-07-27 13:09:24 -0700

    uncommenting stuff left commented by accident

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
-# activityColumnName <- "Activity"
-# subjectIdColumnName <- "Subject ID"
-# 
-# # Read in relevant data
-# subjectTestData <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
-# xTestData <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
-# yTestData <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
-# subjectTrainData <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
-# xTrainData <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
-# yTrainData <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
-# 
-# # Merge the training and the test sets to create one data set
-# testData <- cbind(subjectTestData, xTestData, yTestData)
-# trainingData <- cbind(subjectTrainData, xTrainData, yTrainData)
-# activityData <- rbind(testData, trainingData)
+activityColumnName <- "Activity"
+subjectIdColumnName <- "Subject ID"
+
+# Read in relevant data
+subjectTestData <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+xTestData <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+yTestData <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+subjectTrainData <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+xTrainData <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+yTrainData <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+
+# Merge the training and the test sets to create one data set
+testData <- cbind(subjectTestData, xTestData, yTestData)
+trainingData <- cbind(subjectTrainData, xTrainData, yTrainData)
+activityData <- rbind(testData, trainingData)
 
 # Appropriately label the data set with descriptive variable names, and
 # Extract only the measurements on the mean and standard deviation for each measurement
